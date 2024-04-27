0c9a03df42dc16f6c52c9e1856ffdce925399ea5
KhoiDang1209 <khoibk123123@gmail.com>
2024-04-27 11:26:17 +0700

    new update

--- src/main/java/Controller/Login.java
+++ src/main/java/Controller/Login.java
@@ -3,10 +3,13 @@ package Controller;
 import image.BackGroundScene;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -15,29 +18,50 @@ import org.example.sm.Main;
 import java.io.IOException;
 
 public class Login {
+
     private Stage stage;
     private Scene scene;
-    private AnchorPane anchorPane;
+    @FXML
+    TextField userTextField;
+    @FXML
+    TextField passTextField;
 
-    public void switchtoStudenLogin(ActionEvent event) throws IOException {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("StudentLogin.fxml"));
-            Parent root=fxmlLoader.load();
-            stage=(Stage)((Node) event.getSource()).getScene().getWindow();
-            BackGroundScene backGroundScene=new BackGroundScene();
-            StackPane stackPane=new StackPane(backGroundScene.getBackgroundView(),root);
-            scene=new Scene(stackPane);
+    public void Login(ActionEvent event) throws IOException {
+        String username = userTextField.getText();
+        String password = passTextField.getText();
+        if (username.equals("ITCSIU22266") && password.equals("1") || username.equals("ITCSIU22252") && password.equals("1") ) {
+            FXMLLoader studentplatform = new FXMLLoader(Main.class.getResource("StudentPlatform.fxml"));
+            Parent root = studentplatform.load();
+            StudentPlatform studentPlatform = studentplatform.getController();
+            studentPlatform.displayName(username);
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            BackGroundScene backGroundScene = new BackGroundScene();
+            StackPane stackPane = new StackPane(backGroundScene.getBackgroundView(), root);
+            scene = new Scene(stackPane);
             stage.setScene(scene);
             stage.show();
-    }
-
-    public void Instructor(ActionEvent event) throws IOException {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("InstructorLogin.fxml"));
-            Parent root=fxmlLoader.load();
-            stage=(Stage)((Node) event.getSource()).getScene().getWindow();
-            BackGroundScene backGroundScene=new BackGroundScene();
-            StackPane stackPane=new StackPane(backGroundScene.getBackgroundView(),root);
-            scene=new Scene(stackPane);
+        }
+        else if (username.equals("ins123") && password.equals("1") || username.equals("ins456") && password.equals("1") ) {
+            FXMLLoader studentplatform = new FXMLLoader(Main.class.getResource("StudentPlatform.fxml"));
+            Parent root = studentplatform.load();
+            StudentPlatform studentPlatform = studentplatform.getController();
+            studentPlatform.displayName(username);
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            BackGroundScene backGroundScene = new BackGroundScene();
+            StackPane stackPane = new StackPane(backGroundScene.getBackgroundView(), root);
+            scene = new Scene(stackPane);
             stage.setScene(scene);
             stage.show();
+        }
+        else {
+            // Nếu username hoặc password không đúng, hiển thị thông báo lỗi hoặc thực hiện hành động khác
+            // Ví dụ: Hiển thị một cửa sổ thông báo
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login Failed");
+            alert.setHeaderText(null);
+            alert.setContentText("Invalid username or password. Please try again.");
+            alert.showAndWait();
+
+        }
     }
 }
--- src/main/java/image/BackGroundScene.java
+++ src/main/java/image/BackGroundScene.java
@@ -4,20 +4,23 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class BackGroundScene {
-    Image background = new Image("file:///D:/OOP%20Project/SM/src/main/java/image/background.jpg");
-    public double getBackgroundWidth()
-    {
+    Image background;
+    ImageView backgroundView;
+
+    public BackGroundScene() {
+        // Sử dụng đường dẫn tương đối từ thư mục resources
+        background = new Image(getClass().getResource("/org/example/sm/background.jpg").toExternalForm());
+        backgroundView = new ImageView(background);
+    }
+
+    public double getBackgroundWidth() {
         return background.getWidth();
     }
-    public double getBackgroundHeight()
-    {
+
+    public double getBackgroundHeight() {
         return background.getHeight();
     }
-    ImageView backgroundView;
-    public BackGroundScene()
-    {
-        this.backgroundView=new ImageView(background);
-    }
+
     public ImageView getBackgroundView() {
         return backgroundView;
     }
